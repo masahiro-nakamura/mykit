5d0b4212dcbe917cbccadabbcc4d4a6f02b1b483
masahiro-nakamura <masahiro.nakamura.1992@gmail.com>
2017-12-27 01:44:10 +0900

    register finished

---
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -0,0 +1,32 @@
+const Joi = require('joi')
+
+module.exports = {
+  register (req, res, next) {
+    const schema = {
+      email: Joi.string().email(),
+      password: Joi.string().regex(/^[a-zA-Z0-9]{8,30}$/)
+    }
+    const  {error, value} = Joi.validate(req.body, schema)
+
+    if (error) {
+      switch (error.details[0].context.key){
+        case 'email':
+          res.status(400).send({
+            error: 'You must provide a valid email address.'
+          })
+          break
+        case 'password':
+          res.status(400).send({
+            error: 'You must provide a valid password.'
+          })
+          break
+        default:
+          res.status(400).send({
+            error: 'Invalid registration information.'
+          })
+      }
+    } else {
+      next()
+    }
+  }
+}

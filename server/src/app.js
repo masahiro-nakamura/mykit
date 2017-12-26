366c784d615df712a29b98f64eca1826e3245cde
masahiro-nakamura <masahiro.nakamura.1992@gmail.com>
2017-12-26 23:08:06 +0900

    before adding sequelize

--- server/src/app.js
+++ server/src/app.js
@@ -2,16 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const {sequelize} = require('./models')
+const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
-  res.send({
-    message: 'Hello world!'
+sequelize.sync()
+  .then(() => {
+    app.listen(config.port)
+    console.log(`Servere started on port ${config.port}`)
   })
-})
+
+require('./routes')(app)
 
 app.listen(process.env.PORT || 8081)
